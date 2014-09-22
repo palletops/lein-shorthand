81182f395930614992e876fcb9184fb7ef3d0245
Hugo Duncan <hugo@hugoduncan.org>
2014-09-22 09:15:28 -0400

    Speed up plugin loading
    Avoid the `require` on `leiningen.repl`.  This saves about 1.5s on `lein
    version` when the plugin is active.

--- src/lein_inject/plugin.clj
+++ src/lein_inject/plugin.clj
@@ -2,8 +2,8 @@
   "Plugin to inject namespaces"
   (:require
    [com.palletops.repl-inject :refer [injections]]
+   [leiningen.core.main :as main]
    [leiningen.core.project :as project]
-   [leiningen.repl]
    [robert.hooke :refer [add-hook]]))
 
 (defn injection-profiles
@@ -23,6 +23,11 @@
   [project]
   (project/add-profiles project (injection-profiles project)))
 
+;; We would like to be able to hook a function after the :repl profile
+;; is merged, but that is not very feasible at the moment.
+
+;; We also want to avoid require'ing leiningen.repl just to hook it,as
+;; it can add considerably to lein startup time when not using the repl.
 
 (defn repl-hook
   [task & [project & args]]
@@ -30,8 +35,17 @@
          (project/merge-profiles project [:plugin.lein-inject/injections])
          args))
 
+(defn add-repl-hook  []
+  (when-let [repl (resolve 'leiningen.repl/repl)]
+    (add-hook repl repl-hook)))
+
+(defn resolve-task-hook
+  [f & args]
+  (let [r (apply f args)]
+    (add-repl-hook)
+    r))
+
+
 (defn hooks
   []
-  ;; we would like to be able to hook a function after the :repl
-  ;; profile is merged, but that is not very feasible at the moment.
-  (add-hook #'leiningen.repl/repl repl-hook))
+  (add-hook #'main/resolve-task resolve-task-hook))
