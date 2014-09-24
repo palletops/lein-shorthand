acdde70db0b98220ec2fbb41a45677ac09a12c8e
Hugo Duncan <hugo@palletops.com>
2014-09-24 10:53:37 +0000

    Updated version for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.palletops/lein-shorthand "0.4.0"
+(defproject com.palletops/lein-shorthand "0.4.1-SNAPSHOT"
   :description
   "A leiningen plugin to create clojure namespaces with short names,
    so you can easily call utility functions in the REPL using fully
